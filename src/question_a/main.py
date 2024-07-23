a85cbf01d5c79560fde245fd6161edeccfc2065b
z3nbunny <starfire3d@gmail.com>
2024-07-23 01:25:22 +0000

    question a complete

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,12 @@
 #add import
+from question_a import create_multiplication_table, display_multiplication_table
+
+def multiplication_loop():
+    while True:
+        table = create_multiplication_table()
+        display_multiplication_table(table)
+        cont = input("Keep generating? (y/n): ").strip().lower()
+        if cont != 'y':
+            break
+
+multiplication_loop()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,19 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def create_multiplication_table():
+    multiplication_table = []
+    for i in range(1, 6):
+        row = []
+        for j in range(1, 11):
+            row.append(i * j)
+        multiplication_table.append(row)
+    return multiplication_table
+
+def display_multiplication_table(multiplication_table):
+    for row in multiplication_table:
+        row_string = ""
+        for value in row:
+            row_string += str(value) + " "
+        print(row_string.strip())
