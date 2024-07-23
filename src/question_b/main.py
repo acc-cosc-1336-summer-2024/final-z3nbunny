d64b27db9f47a8f7f486669d3ae0a5715210207e
z3nbunny <starfire3d@gmail.com>
2024-07-23 01:51:57 +0000

    Question b complete

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,4 @@
 #add import
+from question_b import five_numbers
+
+five_numbers()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,16 @@
 #write functions here, don't add input('') statements here!
+
+def five_numbers():
+    numbers = []
+    print("Please enter 5 numbers, one at a time each followed by enter")
+    for i in range(5):
+        get_numbers = int(input("Enter a number: "))
+        numbers.append(get_numbers)
+    print("Lowest number: " + str(min(numbers)))
+    print("Highest number: " + str(max(numbers)))
+    total = 0
+    for j in range(5):
+        total += numbers[j]
+    print("Total of the numbers: " + str(total))
+    average = total / 5
+    print("Average of the numbers: " + str(average))
