9f07134a2e512c0c49565c3c20ace56c0088c4ad
z3nbunny <starfire3d@gmail.com>
2024-07-23 05:12:37 +0000

    working on final exam question d

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,12 @@
 #add import
+
+from Stock import Stock
+
+def single_stock():
+    apple_stock = Stock('AAPL', 'Apple Inc')
+
+    print("Stock Report")
+    print("Company Symbol")
+    print(f"{apple_stock.get_company_name()} {apple_stock.get_symbol()}")
+
+single_stock()
