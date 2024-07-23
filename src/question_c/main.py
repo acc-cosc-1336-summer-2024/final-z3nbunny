c22eb56ba7ef3f969c312790815edf4acdfebb80
z3nbunny <starfire3d@gmail.com>
2024-07-23 04:34:03 +0000

    Question c complete

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,21 @@
 #add import
+
+from question_c import stock_purchase_history
+
+def display_stock_purchase_history():
+    while True:
+        print("Welcome to your Stock Portfolio, please read carefully as the following menu has changed:")
+        print("1 - Display stock purchase history")
+        print("2 - Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == '1':
+            stock_purchase_history()
+        elif choice == '2':
+            print("Exiting... Have good day!")
+            break
+        else:
+            print("Invalid option. Please choose 1 or 2.")
+
+display_stock_purchase_history()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,22 @@
 #write functions here, don't add input('') statements here!
+from stock import Stock
+
+def stock_purchase_history():
+    apple_stock = Stock('AAPL', 'Apple Inc')
+    caterpillar_stock = Stock('CAT', 'Caterpillar')
+    eastman_kodak_stock = Stock('EK', 'Eastman Kodak')
+    google_stock = Stock('GOOG', 'Google')
+    microsoft_stock = Stock('MSFT', 'Microsoft')
+
+    stocks_dictionary = {
+        'AAPL': apple_stock,
+        'CAT': caterpillar_stock,
+        'EK': eastman_kodak_stock,
+        'GOOG': google_stock,
+        'MSFT': microsoft_stock
+    }
+
+    print(f"{'Symbol'} {'Company Name'}")
+
+    for stock in stocks_dictionary.values():
+        print(f"{stock.get_symbol()} {stock.get_company_name()}")
