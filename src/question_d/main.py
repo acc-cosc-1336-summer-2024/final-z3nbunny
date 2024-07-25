8e5ee22600b8316072ae798f4764fdc1a67d0cd6
z3nbunny <starfire3d@gmail.com>
2024-07-25 00:01:53 +0000

    Question d complete

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,12 +1,5 @@
 #add import
 
-from Stock import Stock
-
-def single_stock():
-    apple_stock = Stock('AAPL', 'Apple Inc')
-
-    print("Stock Report")
-    print("Company Symbol")
-    print(f"{apple_stock.get_company_name()} {apple_stock.get_symbol()}")
+from question_d import single_stock
 
 single_stock()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,9 @@
 #write functions here, don't add input('') statements here!
+from Stock import Stock
+
+def single_stock():
+    apple_stock = Stock('AAPL', 'Apple Inc')
+
+    print("Stock Report")
+    print("Company Symbol")
+    print(f"{apple_stock.get_company_name()} {apple_stock.get_symbol()}")
